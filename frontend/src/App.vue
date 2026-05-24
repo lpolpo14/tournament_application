529c0013f6eee9707be7c30464e22eafdda2bc6c
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-05-24 19:54:32 +0300

    Fully initialized a Vue.js project using Vite. Also added Tailwind support. Frontend + Backend is now in full development

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+    <div class="rounded-2xl bg-slate-900 p-8 shadow-xl">
+      <h1 class="text-3xl font-bold">
+        Tournament Manager
+      </h1>
+
+      <p class="mt-4 text-slate-300">
+        Coming Soon!
+      </p>
+    </div>
+  </main>
+</template>
