4b8b27317098e654bf8d9d367c4afa2081693e5a
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-06-09 00:05:32 +0300

    Added models to admin.py

--- backend/apps/tournaments/admin.py
+++ backend/apps/tournaments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tournament,TournamentMatch
+
 
 # Register your models here.
+admin.site.register(TournamentMatch)
+admin.site.register(Tournament)
