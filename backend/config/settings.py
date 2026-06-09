337a41efeba12d3e725df6f79352430d54c4cd13
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-06-09 18:07:33 +0300

    Added CORS Support to app.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'apps.teams',
     'apps.matches',
     'apps.tournaments',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
