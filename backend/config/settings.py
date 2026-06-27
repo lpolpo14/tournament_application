baaf7d7178580e585f37d919e0a779d548e074e4
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-06-27 05:59:03 +0300

    Added CORS and CSRF Safety settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+
+CSRF_TRUSTED_ORIGINS = [
+    os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "http://localhost:5173"),
+]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,8 +68,9 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS", "http://localhost:5173"),
 ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
